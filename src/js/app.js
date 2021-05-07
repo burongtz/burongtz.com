55a51e3b4edccc2de0660c18da4648baed43ac4a
Javier Buron Gtz <javier.buron.gtz@gmail.com>
2021-05-06 20:03:43 -0500

    Add function toggleTheMenu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-console.log('Hola mundo!');
+const toggleTheMenu = () => {
+    const theMenu = document.querySelector('#the-menu');
+    const theMenuToggler = document.querySelector('#the-menu-toggler');
+
+    if (theMenu == null || theMenuToggler == null) {
+        return;
+    }
+
+    theMenuToggler.addEventListener(
+        'click',
+        () => theMenu.classList.toggle('d-block'),
+        false
+    );
+};
+
+(function () {
+    toggleTheMenu();
+})();
